4fc6eeca6f62a69065a204cf684a93d3bdcfc404
Bertus Kruger <bertus.kruger@pertronic.co.nz>
2015-09-03 14:38:53 +1200

    Added skeleton for Custom patterns.

--- src/CustomPattern.cpp
+++ src/CustomPattern.cpp
@@ -2,12 +2,21 @@
 #include "CustomPattern.h"
 #include "pixel_helper.h"
 
+struct CUSTOMPATTERN {
+  // int[255][16][3] Patterns;
+  // int[255] PatternIntervals;
+  int Index;			// Current position in the Animation array
+  int NumberOfPattern;  // Set the number of animations
+};
+
+CUSTOMPATTERN cp;
+
 void ParseCustomPattern(String input, PIXEL_HELPER_CLASS* p_helper) {
 	p_helper->LEDMode = CustomPattern_Mode;
-
 }
 
 void DoCustomPatternMode(PIXEL_HELPER_CLASS* p_helper) {
-	
+    p_helper->SetAll({0.0.0});
+    p_helper->strip.Show();	
 }
 
