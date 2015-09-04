8341c4b29e54c77a7cc2c850542a672c7281d880
Bertus Kruger <krugercreations@gmail.com>
2015-09-04 21:09:43 +1200

    First Working animation

--- src/CustomPattern.cpp
+++ src/CustomPattern.cpp
@@ -7,8 +7,9 @@
 
 struct ANIMATION{
   RgbColor Frames[maxFrames][16];
-  int FrameIntervals[maxFrames];
+  int FrameIntervals[maxFrames] = {0};
   int Index;			// Current position in the Animation array
+  int Cycles;
 };
 
 ANIMATION A1;
@@ -33,47 +34,50 @@ void TestPatternInjector(){
   A1.Frames[0][15] = RgbColor(30,0,0);
 
   //Blue
-  A1.Frames[0][0] = RgbColor(0,0,30);
-  A1.Frames[0][1] = RgbColor(0,0,0);
-  A1.Frames[0][2] = RgbColor(0,0,30);
-  A1.Frames[0][3] = RgbColor(0,0,0);
-  A1.Frames[0][4] = RgbColor(0,0,30);
-  A1.Frames[0][5] = RgbColor(0,0,0);
-  A1.Frames[0][6] = RgbColor(0,0,30);
-  A1.Frames[0][7] = RgbColor(0,0,0);
-  A1.Frames[0][8] = RgbColor(0,0,30);
-  A1.Frames[0][9] = RgbColor(0,0,0);
-  A1.Frames[0][10] = RgbColor(0,0,30);
-  A1.Frames[0][11] = RgbColor(0,0,0);
-  A1.Frames[0][12] = RgbColor(0,0,30);
-  A1.Frames[0][14] = RgbColor(0,0,0);
-  A1.Frames[0][15] =  RgbColor(0,0,30);
+  A1.Frames[1][0] = RgbColor(0,0,30);
+  A1.Frames[1][1] = RgbColor(0,0,0);
+  A1.Frames[1][2] = RgbColor(0,0,30);
+  A1.Frames[1][3] = RgbColor(0,0,0);
+  A1.Frames[1][4] = RgbColor(0,0,30);
+  A1.Frames[1][5] = RgbColor(0,0,0);
+  A1.Frames[1][6] = RgbColor(0,0,30);
+  A1.Frames[1][7] = RgbColor(0,0,0);
+  A1.Frames[1][8] = RgbColor(0,0,30);
+  A1.Frames[1][9] = RgbColor(0,0,0);
+  A1.Frames[1][10] = RgbColor(0,0,30);
+  A1.Frames[1][11] = RgbColor(0,0,0);
+  A1.Frames[1][12] = RgbColor(0,0,30);
+  A1.Frames[1][13] = RgbColor(0,0,0);
+  A1.Frames[1][14] =  RgbColor(0,0,30);
+  A1.Frames[1][15] = RgbColor(0,0,0);
   
   // Set Intervals between changes.
   A1.FrameIntervals[0] = 1000;
   A1.FrameIntervals[1] = 1000;
   
-  A1.Index = 0;
+  A1.Index = maxFrames;
+
+  A1.Cycles = 0;
 }
 
 
 int IncrementIndex(int index,int *intervals,int maxTries ){
     index++;
     maxTries--;
-    if(index > maxFrames){
+    if(index >= maxFrames){
       index = 0;
     }
 
     // Prevent circular increments finding the next non zero frame.
-    if(intervals[index] == 0 && maxFrames >= 0){
+    if(intervals[index] == 0 && maxTries >= 0){
       index = IncrementIndex(index,intervals,maxTries);
     }
     return index;
 }
 
-void SetPixels(NeoPixelBus strip,RgbColor *frame){
+void SetPixels(PIXEL_HELPER_CLASS* p_helper,RgbColor *frame){
   for (int K = 0; K < leds; K++) {
-    strip.SetPixelColor(K,frame[K]);
+    p_helper->strip.SetPixelColor(K,frame[K]);
   }
 }
 
@@ -81,16 +85,20 @@ void SetPixels(NeoPixelBus strip,RgbColor *frame){
 void ParseCustomPattern(String input, PIXEL_HELPER_CLASS* p_helper) {
   p_helper->LEDMode = CustomPattern_Mode;
   TestPatternInjector();
-  Serial.println("Injected"); 
-  A1.Index = 255;
 }
 
 void DoCustomPatternMode(PIXEL_HELPER_CLASS* p_helper) {
-    A1.Index = IncrementIndex(A1.Index,A1.FrameIntervals,maxFrames);
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - p_helper->previousMillis > A1.FrameIntervals[A1.Index]) {
+    p_helper->previousMillis = currentMillis;
+    
+    A1.Index = IncrementIndex(A1.Index,A1.FrameIntervals,maxFrames+20);
     Serial.println(A1.Index); 
     
-    SetPixels(p_helper->strip,A1.Frames[A1.Index]);    
+    SetPixels(p_helper,A1.Frames[A1.Index]);    
 
-    p_helper->strip.Show();	
+    p_helper->strip.Show();
+  }	
 }
 
